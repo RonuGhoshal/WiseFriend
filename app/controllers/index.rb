53192900c9680a99b9078eab89bb99829bb89748
Alivia Blount <aliviablount@gmail.com>
2016-04-07 16:45:50 -0700

    test

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,4 @@
 get '/' do
-  @tweets = TWITTER_CLIENT.search("to:ijessewilliams ", result_type: "recent").take(20).collect do |tweet|
-  "#{tweet.user.screen_name}: #{tweet.text}"
-  end
   erb :'index'
 end
 
