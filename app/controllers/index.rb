602a4a550a22cf95460a411c1c086746716b2be1
Alivia Blount <aliviablount@gmail.com>
2016-04-07 16:21:45 -0700

    We got the tweets, integrated twitter api and .env files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,7 @@
 get '/' do
+  @tweets = TWITTER_CLIENT.search("to:ijessewilliams ", result_type: "recent").take(20).collect do |tweet|
+  "#{tweet.user.screen_name}: #{tweet.text}"
+  end
   erb :'index'
 end
 
